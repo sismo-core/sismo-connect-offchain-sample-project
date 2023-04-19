9c33778fec972f85a4eee073fb586379bec67585
Benjamin A <97291322+Baoufa@users.noreply.github.com>
2023-04-19 13:30:34 +0200

    feat: proof of humanity example

--- src/pages/api/verify-auth-and-claim.ts
+++ src/pages/api/verify-auth-and-claim.ts
@@ -1,34 +1,56 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next';
-import { SismoConnect, SismoConnectServerConfig, AuthType, SismoConnectVerifiedResult } from '@sismo-core/sismo-connect-server';
+import type { NextApiRequest, NextApiResponse } from "next";
+import {
+  SismoConnect,
+  SismoConnectServerConfig,
+  AuthType,
+  SismoConnectVerifiedResult,
+} from "@sismo-core/sismo-connect-server";
 
 const sismoConnectConfig: SismoConnectServerConfig = {
   appId: "0x112a692a2005259c25f6094161007967",
   devMode: {
-    enabled: true, 
-  }
-}
+    enabled: true,
+  },
+};
+
+const userStore = new Map<string, string>();
 
 const sismoConnect = SismoConnect(sismoConnectConfig);
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<void>
+  res: NextApiResponse<{
+    userName: string;
+    userId: string;
+  } | void>
 ) {
   const { response } = req.body;
+
   try {
     const result: SismoConnectVerifiedResult = await sismoConnect.verify(response, {
-      claims: [{groupId: "0xe9ed316946d3d98dfcd829a53ec9822e"}],
-      auths: [{authType: AuthType.VAULT}],
-      signature: {message: "0x1234568"}
+      claims: [{ groupId: "0x682544d549b8a461d7fe3e589846bb7b" }],
+      auths: [{ authType: AuthType.VAULT }],
+      signature: {
+        message: "",
+        isSelectableByUser: true,
+      },
     });
-    console.log("Response verified:", result.response);
-    // vaultId = hash(userVaultSecret, appId). 
-    // the vaultId is an app-specific, anonymous identifier of a vault
-    console.log("VaultId: ", result.getUserId(AuthType.VAULT))
-    res.status(200).send();
+
+    // the userId is an app-specific, anonymous identifier of a vault
+    // userId = hash(userVaultSecret, appId).
+    const userId = result.getUserId(AuthType.VAULT);
+    const signedMessage = result.getSignedMessage();
+
+    // save the user name (signedMessage) in the user store DB
+    userStore.set(userId, signedMessage);
+
+    res.status(200).send({
+      userId: userId,
+      userName: signedMessage,
+    });
   } catch (e: any) {
     console.error(e);
-    res.status(400).send();
+    res.status(400).send(null);
   }
 }
--- src/pages/off-chain/auth-and-claim.tsx
+++ src/pages/off-chain/auth-and-claim.tsx
@@ -4,7 +4,7 @@ import {
   SismoConnectButton,
   SismoConnectClientConfig,
   SismoConnectResponse,
-  AuthType
+  AuthType,
 } from "@sismo-core/sismo-connect-react";
 import axios from "axios";
 import { useState } from "react";
@@ -16,12 +16,13 @@ export const sismoConnectConfig: SismoConnectClientConfig = {
     enabled: true,
     devGroups: [
       {
-        groupId: "0xe9ed316946d3d98dfcd829a53ec9822e",
+        groupId: "0x682544d549b8a461d7fe3e589846bb7b",
         // Add your dev addresses here to become eligible in the DEV env
         data: [
           "0x2b9b9846d7298e0272c61669a54f0e602aba6290",
           "0xb01ee322c4f028b8a6bfcd2a5d48107dc5bc99ec",
-          "0x855193BCbdbD346B423FF830b507CBf90ecCc90B"
+          "0x855193BCbdbD346B423FF830b507CBf90ecCc90B",
+          "0x938f169352008d35e065F153be53b3D3C07Bcd90",
         ],
       },
     ],
@@ -32,48 +33,74 @@ export default function OffChainAuthAndClaim() {
   const [verifying, setVerifying] = useState(false);
   const [error, setError] = useState(null);
   const [isVerified, setIsVerified] = useState(false);
+  const [userInput, setUserInput] = useState("");
+
+  const [user, setUser] = useState({
+    id: "",
+    name: "",
+  });
 
   const verify = async (response: SismoConnectResponse) => {
     setVerifying(true);
-    console.log(response)
+    console.log(response);
     try {
-        await axios.post(`/api/verify-auth-and-claim`, {
-            response,
-        })
-        setIsVerified(true);
+      const res = await axios.post(`/api/verify-auth-and-claim`, {
+        response,
+      });
+      setUser({
+        id: res.data.userId,
+        name: res.data.userName,
+      });
+      setIsVerified(true);
     } catch (e) {
-        setError("Invalid response")
-        console.error(e);
+      setError("Invalid response");
+      console.error(e);
     } finally {
-        setVerifying(false);
+      setVerifying(false);
     }
+  };
+
+  function onUserInput(e) {
+    setUserInput(e.target.value);
   }
+
 
   return (
     <Container>
-        <Title>
-            Auth and Claim
-        </Title>
-        {
-            !isVerified ?
-            <>
-                <SismoConnectButton
-                    config={sismoConnectConfig}
-                    auths={[{authType: AuthType.VAULT}]}
-                    claims={[{ groupId: "0xe9ed316946d3d98dfcd829a53ec9822e" }]}
-                    signature={{message: "0x1234568"}}
-                    onResponse={(response: SismoConnectResponse) => verify(response)}
-                    verifying={verifying}
-                    callbackPath={"/off-chain/auth-and-claim"}
-                    overrideStyle={{marginBottom: 10}}
-                />
-                <>
-                {error}
-                </>
-            </>
-            :
-            "Response verified!"
-        }
+      {!isVerified ? (
+        <>
+          <Title>Are you a human?</Title>
+
+          {!verifying && (
+            <div className="input-group">
+              <label htmlFor="userName">Gimme you name</label>
+              <input id="userName" type="text" value={userInput} onChange={onUserInput} />
+            </div>
+          )}
+
+          <SismoConnectButton
+            config={sismoConnectConfig}
+            auths={[{ authType: AuthType.VAULT }]}
+            claims={[{ groupId: "0x682544d549b8a461d7fe3e589846bb7b" }]}
+            signature={{ message: userInput, isSelectableByUser: true }}
+            onResponse={(response: SismoConnectResponse) => verify(response)}
+            verifying={verifying}
+            callbackPath={"/off-chain/auth-and-claim"}
+          />
+          <>{error}</>
+        </>
+      ) : (
+        <>
+          <Title>Yes you are</Title>
+          <div className="profile-container">
+            <div>
+              <h2>User Profile</h2>
+              <p>UserName: {user.name}</p>
+              <p>UserId: {user.id}</p>
+            </div>
+          </div>
+        </>
+      )}
     </Container>
   );
 }
